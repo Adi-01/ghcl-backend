28a560f601c46fba2c451dfc67f89d24b25d921d
Aditya <pushpamaditya@gmail.com>
2026-03-26 22:02:50 +0530

    initial commit

--- ghcl_server/settings.py
+++ ghcl_server/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Application definition
