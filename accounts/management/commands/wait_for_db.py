55be9dbd51e9c68e7ad4bc8b3b608e80cf1c52e1
Aditya <pushpamaditya@gmail.com>
2026-04-04 00:25:53 +0530

    fixing auth

---
+++ accounts/management/commands/wait_for_db.py
@@ -0,0 +1,22 @@
+import time
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Django command to pause execution until the database is fully ready"""
+
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for database to wake up...')
+        db_conn = None
+        
+        while not db_conn:
+            try:
+                # Try to get the connection and open a cursor
+                db_conn = connections['default']
+                db_conn.cursor()
+            except OperationalError:
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('✅ Database available!'))
--- ghcl_server/settings.py
+++ ghcl_server/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'ghcl_server.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
-        conn_max_age=600,
+        conn_max_age=0,
         conn_health_checks=True,
     )
 }
